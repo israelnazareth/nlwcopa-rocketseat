c8e6e3ff64316db3f0a49827ecbe1a3e14bbd2f7
Israel Nazareth <israel.fab@hotmail.com>
2022-11-06 23:14:52 -0300

    creating routes on mobile screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,12 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { AuthContextProvider } from './src/contexts/AuthContext'; 
 
-import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
@@ -19,7 +19,7 @@ export default function App() {
           backgroundColor='transparent'
           translucent
         />
-        { fontsLoaded ? <SignIn /> : <Loading />}
+        { fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );

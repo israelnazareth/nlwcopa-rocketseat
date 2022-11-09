e3b3363f6fae01d5ec78d7ef50380bbe8c94804b
Israel Nazareth <israel.fab@hotmail.com>
2022-11-09 20:57:02 -0300

    adds meta data on _document.tsx

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -7,6 +7,11 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        <meta property="og:image" content="https://i.imgur.com/DVOJBlb.png" />
+        <meta name="description" content="Landing page para criação de bolão da World Cup 2022" />
+        <meta property="og:title" content="NLW Copa Rocketseat" />
+        <meta property="og:url" content="https://nlwcopa-rocketseat.vercel.app/" />
+        <title>NLW Copa - Trilha Ignite | Rocketseat</title>
       </Head>
       <body className='bg-gray-900 bg-app bg-no-repeat bg-cover'>
         <Main />
